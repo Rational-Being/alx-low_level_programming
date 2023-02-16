81bf037c2146b7d119ff3b2524ef3fb16aaafed7
Rational-Being <overcomer021@gmail.com>
2023-02-16 12:44:48 -0800

    getting complicated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.>
+
+/**
+ * main- This is the main function description
+ *
+ * Return: 0 for successful exit
+ */
+
+int main (void)
+{
+	puts ("\"Programming is like bulding a multiliingual puzzle");
+	return (0)
+}
+
