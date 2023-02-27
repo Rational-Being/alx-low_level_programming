f96ca46a140d2303a3c0610e9cccb8a349f31255
Rational-Being <overcomer021@gmail.com>
2023-02-27 05:34:16 -0800

    created files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: pointer to an int
+ * @b: pointer to ither int
+ * *REturn:nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp - *a;
+	*a = *b;
+	*b = tmp;
+}
+
