6034b2ad152fa9e085999f17dec23e2b82c0b77c
Rational-Being <overcomer021@gmail.com>
2023-04-07 07:32:29 -0700

    i think i resiolved all, a good nap is good

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,4 +1,4 @@
-#ifdef __MAIN_H_
+#ifndef __MAIN_H_
 #define __MAIN_H__
 
 unsigned int binary_to_uint(const char *b);
@@ -8,6 +8,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get _endianness(void);
+int get_endianness(void);
 
 #endif
