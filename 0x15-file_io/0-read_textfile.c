a63c9cec23df238a95119cb720d93d6b7b4ad07a
Rational-Being <overcomer021@gmail.com>
2023-04-12 04:26:01 -0700

    just begining

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * read_textfile: a funtion that reads a text file and prints it to the standard outout
+ * @filename: the name of file
+ * @letters: the number of letters to be printed and read
+ * Return: the actual number of letters it prints and reads
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int 
