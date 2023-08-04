05731be4ad8daa567ace36e830ad2ca9d89c6814
Rational-Being <overcomer021@gmail.com>
2023-08-04 09:03:01 -0700

    using a new approach

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,9 @@
-#!bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
+#!/bin/bash
+
+c_files=$(ls *.c)
+for file in $c_files
+do
+    gcc -c $file -o ${file%.c}.o
+done
+ar rcs liball.a *.o
+
