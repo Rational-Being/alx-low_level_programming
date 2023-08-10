3a92f1ce14a76b29723d846b3a17bc13da89d6e2
Rational-Being <overcomer021@gmail.com>
2023-08-09 21:58:53 -0700

    couldnt free memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- * Return:
+ * alloc_grid - funtion creates 2d array with malloc
+ * @width: integer width
+ * @height: integer height
+ * Return: returns array pointer
  */
 
 int **alloc_grid(int width, int height)
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array = malloc (height * sizeof(*array));
+	array = malloc(height * sizeof(*array));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		if (array[i] == NULL)
 			return (NULL);
 	}
-	
+
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			array[i][j] = 0;
