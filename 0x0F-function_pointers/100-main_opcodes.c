1ec44d9eef6d192439b8d90f5a96e840a36b94d9
Rational-Being <overcomer021@gmail.com>
2023-08-16 15:02:56 -0700

    Question 3?

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - a program that prints the opcodes of its own mian fiunction
+ * @argc: command line argument passed
+ * @argv: argument counts
+ * Return: 0 on sucess
+ */
 int main(int argc, char *argv[])
 {
 	int bytes, i;
@@ -9,27 +15,24 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
-	
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
 	{
 		printf("Error\n");
-		return 2;
+		return (2);
 	}
-	
 	main_ptr = (unsigned char *)main;
-
 	for (i = 0; i < bytes; i++)
 	{
 		printf("%02x", *(main_ptr + i));
-		if (i != bytes -1)
+		if (i != bytes - 1)
 			printf(" ");
 	}
 
 	printf("\n");
 
-	return 0;
+	return (0);
 }
