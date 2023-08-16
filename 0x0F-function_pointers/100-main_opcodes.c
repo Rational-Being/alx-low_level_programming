d19a3f5d219a13176c83a29d9c82d931558e463a
Rational-Being <overcomer021@gmail.com>
2023-08-16 14:47:02 -0700

    th function is good but is not giving he same output as julien's

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	unsigned char *main_ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return 2;
+	}
+	
+	main_ptr = (unsigned char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", *(main_ptr + i));
+		if (i != bytes -1)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return 0;
+}
