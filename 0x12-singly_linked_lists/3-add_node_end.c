1db8140e6790e913430c7a47a1f74f3845addb81
Rational-Being <overcomer021@gmail.com>
2023-08-24 20:54:11 -0700

    created and freed memory for a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,4 +8,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	lists_t *
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *last = *head;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new_node;
+
+	return (new_node);
+}
