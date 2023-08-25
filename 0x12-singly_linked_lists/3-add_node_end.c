8acc68aed19e5eedcf9c2dfb141062739a656e70
Rational-Being <overcomer021@gmail.com>
2023-08-24 20:48:31 -0700

    trying something out

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * add_node_end - funtion that adds a nodeat the end
+ * @head: firts elment
+ * @str: string to add
+ * Retunr: pointer to new wlwnwmt, null onfiurre
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	lists_t *
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t 
+ * @head: pointer to head
+ * Returns: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *temp;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp->str);
+		free(temp);
+	}
+}
