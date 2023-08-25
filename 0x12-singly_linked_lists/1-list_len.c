c637d357a5f06ac4e6c987e87ce3a84026944fcf
Rational-Being <overcomer021@gmail.com>
2023-08-24 19:38:59 -0700

    whats wrong

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the no of elements ina a list
+ * @h: singly linled list
+ * Return: number of elements in the list
+ */
+
+size_t list_len(const list_t &h)
+{
+	size_t no_nodes;
+
+	no_nodes = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		no_nodes;
+	}
+	return (no_nodes);
+}
