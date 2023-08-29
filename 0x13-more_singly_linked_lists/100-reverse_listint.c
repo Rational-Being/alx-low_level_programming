beae7850845bd49ceb487c5d56cac911c8a0492c
Rational-Being <overcomer021@gmail.com>
2023-08-29 05:58:11 -0700

    while memory leakage in 100.c files

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - function reverses linked list
+ * @head: pointer to head
+ * return: pointer to the first node of a reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current = NULL;
+	listint_t *before = NULL;
+
+	if (*head == NULL)
+		return (NULL);
+
+	while (before != NULL)
+	{
+		current = (*head)->next;
+		(*head)->next = before;
+		before = *head;
+		*head = current;
+	}
+
+	(*head)->next = before;
+	return (*head);
+}
