70e26bde480155ec0f1b0b99efd7c326b74984f5
Rational-Being <overcomer021@gmail.com>
2023-09-04 11:23:53 -0700

    rearraging thecode for chacker

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
 
 	txtw = write(fd, text_content, len);
-	if (text_content == NULL)
-		text_content = "";
 
 	if (txtw == -1)
 		return (-1);
