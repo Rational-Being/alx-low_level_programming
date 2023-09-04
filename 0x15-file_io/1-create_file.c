6d52584c9484b46dba57cdfdff5728d5236c9d6c
Rational-Being <overcomer021@gmail.com>
2023-09-04 11:25:46 -0700

    rearraging thecode for chacker

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
 		text_content = "";
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
@@ -27,6 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (txtw == -1)
 		return (-1);
+	}
 
 	close(fd);
 	return (1);
