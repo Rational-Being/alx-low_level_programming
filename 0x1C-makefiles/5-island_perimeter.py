68b4964236df5a44e95c1fc00dfbc21dfaa45d7b
Rational-Being <overcomer021@gmail.com>
2023-12-28 13:02:39 +0100

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculates the Island's Perimeter
+    Args:
+        grid (_type_): an array 
+    """
+    
+    perimeter = 0
+    row = len(grid)
+    col = len(grid[0])
+    
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                perimeter += 4
+                
+                # if right cell is land or 1 subtract two
+                if j < col -1 and grid[i][j+1] == 1:
+                    perimeter -= 2
+                
+                #if cell below is also land, subtract two
+                if i < row -1 and grid[i + 1][j] == 1:
+                    perimeter -= 2
+                    
+    return perimeter
