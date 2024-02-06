36f4578d57ad466f827fb194a0e052f90193617d
Rational-Being <overcomer021@gmail.com>
2024-02-06 07:51:16 +0100

    first commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - a funtion that searches for a value oin an array of integer
+ * @array: array passed
+ * @size: size of te array
+ * @value: value to serc for 
+ * Return: first index were value os located otherwise -1 for value not exist
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i = 0;
+
+	if (!array || size == 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("VAlue checked array[%d] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,4 @@
+In this folder, I will about learn about:
+Search algorithm
+Space complexity
+
